b6c7c7084384680aa882ff1d2d6cb8514da461a9
ipnatt <ipnatt@gmail.com>
2020-12-04 16:08:58 -0800

    testing

--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -16,23 +16,26 @@ class Interface
 
     def sign_up
         username = prompt.ask("Welcome, fresh meat! What's your name?")
-        return self.sign_in if Player.find_by(name: username)
-        @player = Player.create({name: username})
+        if Player.find_by(name: username)
+            return self.sign_in
+        else
+        @player = Player.create({name: username, heart: 3})
+        end
         #new user gets added to db
     end
 
     def sign_in
-        player = prompt.ask("Oh i remember you! What was your name again? ")
-        @player = Player.find_by(name: player)
+        player = prompt.ask("Oh, I remember you! Press enter to jump in to the game.")
+        @player = Player.find_by(name: player, heart: 3)
         @player.nil? ? new_player : @player
     end
 
     def start 
-        prompt.select("lets play this game",["Start"])
+        prompt.select("Let's play x game",["Start"])
     end
     def player_menu
         player_menu_arr = ["New Game", "How TO Play", "Quit"]
-        player_input = prompt.select("Welcome back #{@player.name}, please select one of these options") do |menu|
+        player_input = prompt.select("Welcome #{@player.name}, please select one of these options") do |menu|
             player_menu_arr.each_with_index do |choice|
             menu.choice choice
             end
@@ -53,36 +56,31 @@ class Interface
     end  
     def main_menu   
         main_menu_arr = ["Sign up","Sign in"]
-        prompt.select("lets get you into game") do |menu|
+        prompt.select("Sign Up if you're a new player, and Sign In if you're a returning player.") do |menu|
             main_menu_arr.each_with_index do |choice, index|
                 menu.choice choice, index
             end
         end
     end
     def game_menu
-      game_menu_arr = ["Enter Room","Health" ,"Quit"]
-      game_input = prompt.select("hey #{@player.name}, Are you brave enought to Enter the room?") do |menu|
+      game_menu_arr = ["Enter Room","Check Health" ,"Quit"]
+      game_input = prompt.select("Hey #{@player.name}, Are you brave enough to enter the room?") do |menu|
              game_menu_arr.each_with_index do |choice|
              menu.choice choice
             end
         end
-        menu_choice game_input
+        game_choice game_input
     end
     def game_choice(choice)
         case choice 
          when "Enter Room"
             puts "boo"
-         when "Health"
-            puts "bar"
+         when "Check Health"
+            puts "❤️ "" #{@player.heart}"
          when "Quit"
             puts "die"
          else
             puts "bye"
         end
     end
-
-    room menu=room description/turn on flashlight/quit
-    
-    
-  
 end
